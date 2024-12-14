e422cce7e37492cc762995831531d576fa33cbd2
Kara Woo <woo.kara@gmail.com>
2024-12-14 00:36:17 -0800

    simplify

--- day14.R
+++ day14.R
@@ -13,53 +13,17 @@ velocities <- str_extract_all(dat$vel, "-?\\d+") |>
 
 dims <- c(101, 103)
 
-Robot <- R6Class(
-  "Robot",
-  private = list(
-    vel = NULL,
-    dims = NULL,
-    one_move = function() {
-      pos <- self$pos + private$vel
-      for (i in seq_along(pos)) {
-        if (pos[i] > private$dims[i] - 1) {
-          pos[i] <- pos[i] - private$dims[i]
-        } else if (pos[i] < 0) {
-          pos[i] <- private$dims[i]  + pos[i]
-        }
-      }
-      self$pos <- pos
-    }
-  ),
-  public = list(
-    pos = NULL,
-    initialize = function(start, vel, dims) {
-      self$pos <- start
-      private$vel <- vel
-      private$dims <- dims
-    },
-    move = function(times) {
-      replicate(times, private$one_move())
-      self$pos
-    }
-  )
-)
-
-find_robot <- function(start, vel, dims, times = 100) {
-  r <- Robot$new(start, vel, dims)
-  r$move(times)
-}
-
 find_robots <- function(starts, velocities, dims, times = 100) {
-  end_pos <- end_pos <- map2(
+  end_pos <- map2(
     starts,
     velocities,
-    find_robot,
+    \(x, y, dims, times) (x + y * times) %% dims,
     dims = dims,
     times = 100
   )
   do.call(rbind, end_pos) |>
-    as_tibble() |>
-    rename(x = V1, y = V2)
+    as_tibble(.name_repair = "unique") |>
+    rename(x = "...1", y = "...2")
 }
 
 ## part 1
