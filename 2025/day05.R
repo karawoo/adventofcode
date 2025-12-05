bd70e040129a9512b0ec139a04ca180f875bb479
Kara Woo <woo.kara@gmail.com>
2025-12-04 22:15:42 -0800

    day 5

---
+++ 2025/day05.R
@@ -0,0 +1,40 @@
+dat <- readLines("input05.txt")
+
+ranges <- dat[1:(which(dat == "") - 1)] |>
+  strsplit("-") |>
+  lapply(as.numeric)
+
+ingredients <- as.numeric(dat[(which(dat == "") + 1):length(dat)])
+
+## part 1
+
+count <- 0
+for (i in ingredients) {
+  for (j in ranges) {
+    if (i >= j[1] && i <= j[2]) {
+      count <- count + 1
+      break
+    }
+  }
+}
+count
+
+## part 2
+
+sorted_ranges <- ranges[order(sapply(ranges, `[[`, 1))]
+highest <- 0
+count <- 0
+
+for (i in sorted_ranges) {
+  if (i[2] <= highest) {
+    next
+  } else if (i[1] > highest) {
+    count <- count + (i[2] - i[1]) + 1
+  } else {
+    count <- count + (i[2] - (highest))
+  }
+  if (i[2] > highest) {
+    highest <- i[2]
+  }
+}
+count
