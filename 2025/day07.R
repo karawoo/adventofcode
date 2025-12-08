07555adaae6a209f643918cafb569554bca58fe4
Kara Woo <woo.kara@gmail.com>
2025-12-07 23:35:23 -0800

    much simpler day 7

--- 2025/day07.R
+++ 2025/day07.R
@@ -1,30 +1,32 @@
 library("readr")
+options(scipen = 9999)
 
-file <- "example.txt"
-# file <- "input07.txt"
-dat <- read_fwf(file, col_positions = fwf_widths(rep(1, nchar(readLines(file)[1])))) |>
+file <- "input07.txt"
+dat <- read_fwf(
+  file,
+  col_positions = fwf_widths(rep(1, nchar(readLines(file)[1])))
+) |>
   as.matrix()
 
-## part 1
-
 count <- 0
+tl <- vector(mode = "numeric", length = ncol(dat))
+tl[which(dat == "S", arr.ind = TRUE)[, "col"]] <- 1
+
 for (row in seq_len(nrow(dat))) {
   for (col in seq_along(dat[row, ])) {
     if (dat[row, col] == "^") {
-      if (dat[row - 1, col] == "|") {
-        dat[row, col - 1] <- "|"
-        dat[row, col + 1] <- "|"
+      if (tl[col] > 0) {
         count <- count + 1
-      } else {
-        if (dat[row - 1, col] %in% c("S", "|")) {
-          dat[row, col] <- "|"
-        }
-      }
-    } else {
-      if (isTRUE(dat[row - 1, col] %in% c("S", "|"))) {
-        dat[row, col] <- "|"
+        tl[col - 1] <- tl[col - 1] + tl[col]
+        tl[col + 1] <- tl[col + 1] + tl[col]
+        tl[col] <- 0
       }
     }
   }
 }
+
+## part 1
 count
+
+## part 2
+sum(tl)
