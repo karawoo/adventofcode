413348ff4a0c97d55112f9a34ac8cdc078ccc756
Kara Woo <woo.kara@gmail.com>
2025-12-07 22:58:41 -0800

    remove non working stuff

--- 2025/day07.R
+++ 2025/day07.R
@@ -1,6 +1,4 @@
 library("readr")
-library("dplyr")
-library("igraph")
 
 file <- "example.txt"
 # file <- "input07.txt"
@@ -30,54 +28,3 @@ for (row in seq_len(nrow(dat))) {
   }
 }
 count
-
-## write.table(dat, "tmp.txt", quote = FALSE, sep = "", row.names = FALSE, col.names = FALSE)
-
-## part 2
-
-indArray <- function(i, dat) {
-  matrix(seq_len(ncol(dat) * nrow(dat)), ncol = ncol(dat), nrow = nrow(dat))[i[1], i[2]]
-}
-
-splitters <- which(dat == "^")
-edges <- data.frame(from = c(), to = c())
-
-# replace ^ with an index so we can keep track of vertices
-dat[which(dat == "^")] <- which(dat == "^")
-
-## write for debugging
-## dat_out <- stringr::str_pad(dat, 3, "both", " ")
-## dat_out <- matrix(dat_out, ncol = ncol(dat), nrow = nrow(dat))
-## write.table(dat_out, "tmp2.txt", sep = "", quote = FALSE, row.names = FALSE, col.names = FALSE)
-
-# also need dummy rows at the bottom
-dat[nrow(dat), ] <- rep("end", ncol(dat))
-
-for (row in seq_len(nrow(dat) - 1)) {
-  for (col in seq_along(dat[row, ])) {
-    if (grepl("\\d+", dat[row, col]) && dat[row - 1, col] == "|") {
-      # if (dat[row, col] == "155") browser()
-      # what splitters does this beam connect, if any?
-      l <- dat[(row + 1):nrow(dat), col - 1]
-      r <- dat[(row + 1):nrow(dat), col + 1]
-      edges <- rbind(edges, data.frame(from = dat[row, col], to = l[which(grepl("\\d+", l))[1]]))
-      edges <- rbind(edges, data.frame(from = dat[row, col], to = r[which(grepl("\\d+", r))[1]]))
-      # also add edges to the end of the manifold
-      l <- paste0(l, collapse = "")
-      r <- paste0(r, collapse = "")
-      if (grepl("^\\|*end", l)) {
-        edges <- rbind(edges, data.frame(from = dat[row, col], to = "end"))
-      }
-      if (grepl("^\\|*end", r)) {
-        edges <- rbind(edges, data.frame(from = dat[row, col], to = "end"))
-      }
-    }
-  }
-}
-
-edges <- edges |>
-  filter(!is.na(to))
-
-g <- graph_from_data_frame(edges, vertices = c(splitters, "end", "S"))
-
-length(all_simple_paths(g, from = "115"))
